bad09982959e79d311d1ccc5588174f79119c346
Scott Caruso <scottcaruso@fullsail.edu>
2013-01-10 20:42:42 -0500

    Added all details for 3rd subclass.

--- aoc2/Cars.h
+++ aoc2/Cars.h
@@ -9,6 +9,12 @@
 #import <Foundation/Foundation.h>
 #import "Toys.h"
 
-@interface Cars : Toys
+@interface Cars: Toys
+
+//property to determine if this is a special edition or not
+@property NSString* whichEditionIsThis;
+
+//override the cost if a special edition is being sold
+-(void)costToPurchaseToy;
 
 @end
--- aoc2/Cars.m
+++ aoc2/Cars.m
@@ -6,8 +6,60 @@
 //  Copyright (c) 2013 Scott Caruso. All rights reserved.
 //
 
-#import "Cars.m"
+#import "Cars.h"
 
 @implementation Cars
+
+@synthesize whichEditionIsThis;
+
+//Base "Blocks" init
+-(id)init
+{
+    self = [super init];
+    if (self != nil)
+    {
+        [self setRetailPrice:19.95];
+        [self setWhichEditionIsThis:@"Limited"];
+    }
+    return self;
+}
+
+-(void)costToPurchaseToy;
+{
+    if (whichEditionIsThis == @"Regular")
+    {
+        NSLog(@"There is no additional charge for %@ edition toys.",whichEditionIsThis);
+        [super costToPurchaseToy];
+    }
+    else if (whichEditionIsThis == @"Limited")
+    {
+        NSLog(@"We require insurance of $9.99 be purchased for all Limited edition shipments.");
+        float totalPrice;
+        totalPrice = self.retailPrice + 9.99;
+        float totalCost;
+        totalCost = totalPrice + self.priceToShip;
+        NSString *priceWithInsurance = [NSString stringWithFormat:@"%.02f",totalPrice];
+        NSString *formattedShipping = [NSString stringWithFormat:@"%.02f",self.priceToShip];
+        NSString *formattedCost = [NSString stringWithFormat:@"%.02f",totalCost];
+        NSLog(@"With the mandatory %@ edition insurance purchase, the total price of this item is $%@. The total cost to the customer is $%@, after a $%@ shipping cost.",whichEditionIsThis,priceWithInsurance,formattedCost,formattedShipping);
+    }
+    else if (whichEditionIsThis == @"Special")
+    {
+        NSLog(@"We require insurance of $3.99 be purchased for all %@ edition shipments.",whichEditionIsThis);
+        float totalPrice;
+        totalPrice = self.retailPrice + 3.99;
+        float totalCost;
+        totalCost = totalPrice + self.priceToShip;
+        NSString *priceWithInsurance = [NSString stringWithFormat:@"%.02f",totalPrice];
+        NSString *formattedShipping = [NSString stringWithFormat:@"%.02f",self.priceToShip];
+        NSString *formattedCost = [NSString stringWithFormat:@"%.02f",totalCost];
+        NSLog(@"With the mandatory %@ edition insurance purchase, the total price of this item is $%@. The total cost to the customer is $%@, after a $%@ shipping cost.",whichEditionIsThis,priceWithInsurance,formattedCost,formattedShipping);
+    }
+    else
+    {
+        NSLog(@"A valid edition was not entered. Reverted to standard shipping rates.");
+        [super costToPurchaseToy];
+    }
+}
 
 @end
