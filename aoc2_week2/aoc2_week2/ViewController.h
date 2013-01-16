48204b2d592f507cf5350a4c439e843c070c4a0c
Scott Caruso <scottcaruso@fullsail.edu>
2013-01-16 12:45:56 -0500

    Added all UI elements to viewcontroller header and connected in UIB

--- aoc2_week2/aoc2_week2/ViewController.h
+++ aoc2_week2/aoc2_week2/ViewController.h
@@ -11,7 +11,41 @@
 
 @interface ViewController : UIViewController
 {
+    //creating IBOutlets for all Main UI elements
+    //topheader
     IBOutlet UILabel *toyFactoryHeader;
+    //Main left-side buttons
+    IBOutlet UIButton *teddyBearButton;
+    IBOutlet UIButton *blocksButton;
+    IBOutlet UIButton *carsButton;
+    //text fields
+    IBOutlet UITextField *teddyBearTextField;
+    IBOutlet UITextField *blocksTextField;
+    IBOutlet UITextField *carsTextField;
+    //textareas
+    IBOutlet UITextView *teddyBearTextView;
+    IBOutlet UITextView *blocksTextView;
+    IBOutlet UITextView *carsTextView;
+    //steppers & other modifiers & labels
+    //teddy bears
+    IBOutlet UIStepper *teddyBearNumberStepper;
+    IBOutlet UIStepper *teddyBearSaleStepper;
+    IBOutlet UILabel *teddyBearNumberStepperLabel;
+    IBOutlet UILabel *teddyBearSaleStepperLabel;
+    //blocks
+    IBOutlet UIStepper *blocksNumberStepper;
+    //weight accepts numbers only
+    IBOutlet UITextField *blocksWeightEntry;
+    IBOutlet UILabel *blocksNumberStepperLabel;
+    IBOutlet UILabel *blocksWeightEntryLabel;
+    //cars
+    IBOutlet UIStepper *carsNumberStepper;
+    IBOutlet UIButton *carsEditionButton;
+    IBOutlet UILabel *carsNumberStepperLabel;
+    IBOutlet UILabel *carsEditionButtonLabel;
+    //Need to add second cars element and label
+    
+    
     
     UILabel *baseClasses;
     UILabel *teddyBearBase;
