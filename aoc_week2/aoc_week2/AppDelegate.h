6093622db630b8155012225b1b31c21135bc5aa0
Scott Caruso <scottcaruso@fullsail.edu>
2013-01-16 04:32:20 -0500

    Finally got everything not broken.

---
+++ aoc_week2/aoc_week2/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  aoc_week2
+//
+//  Created by Scott Caruso on 1/16/13.
+//  Copyright (c) 2013 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
