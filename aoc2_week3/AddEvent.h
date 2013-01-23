ec40baf72baf047de36a0c8291a30e394f138a67
Scott Caruso <scottcaruso@fullsail.edu>
2013-01-22 22:48:55 -0500

    Added and stubbed out second view.

---
+++ aoc2_week3/AddEvent.h
@@ -0,0 +1,13 @@
+//
+//  AddEvent.h
+//  aoc2_week3
+//
+//  Created by Scott Caruso on 1/22/13.
+//  Copyright (c) 2013 Scott Caruso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEvent : UIViewController
+
+@end
---
+++ aoc2_week3/aoc2_week3/AddEvent.m
@@ -0,0 +1,38 @@
+//
+//  AddEvent.m
+//  aoc2_week3
+//
+//  Created by Scott Caruso on 1/22/13.
+//  Copyright (c) 2013 Scott Caruso. All rights reserved.
+//
+
+#import "AddEvent.h"
+
+@interface AddEvent ()
+
+@end
+
+@implementation AddEvent
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
