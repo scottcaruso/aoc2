86988298ea8e3e0a421ae263a52660d7d7c347f4
Scott Caruso <scottcaruso@fullsail.edu>
2013-01-29 22:12:33 -0500

    Added some relevant comments.

--- aoc2_week3/aoc2_week3/ViewController.h
+++ aoc2_week3/aoc2_week3/ViewController.h
@@ -17,10 +17,13 @@
     UISwipeGestureRecognizer *swipeRight;
 }
 
+//What to do when the Save button is clicked
 -(IBAction)onSaveButton: (id)sender;
 
+//Does what it sounds like - appends a new event to the main view
 -(void)appendNewEvent:(NSString *)description date:(NSString *)dateString;
 
+//See the implementation file; this essentially makes the date a friendly string.
 -(NSString *)convertTimeToLocalRegion:(NSDate *)dateEntered;
 
 @end
